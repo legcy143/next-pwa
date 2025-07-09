048e124129aff7353f26d6e346d21ac73b46d760
prince <legcy143@gmail.com>
2025-07-09 19:12:31 +0530

    simple pwa app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,7 @@
-"use client"
-import { Button, ToastProvider, addToast } from '@heroui/react'
-import React, { useEffect } from 'react'
+import React from 'react'
 
-export default function Home() {
-
-  useEffect(() => {
-
-
-  }, [])
-
-  const register = () => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sevice-worker.js").then(e => {
-        console.log("register done");
-      }).catch(e => {
-        console.log("register error ",e)
-      })
-    }
-
-    window.addEventListener('beforeinstallprompt', (e) => {
-      e.preventDefault();
-    });
-  }
-
-  const unRegister = () => {
-
-
-    if ('serviceWorker' in navigator) {
-      console.log(navigator.serviceWorker)
-      navigator.serviceWorker.getRegistration().then((registrations) => {
-        console.log("registrations : ", registrations)
-        // registrations?.unregister().then((bool) => {
-        //   console.log("Service worker ", bool)
-        // })
-      })
-    }
-
-  }
-
-
+export default function page() {
   return (
-    <div>
-
-      <h1>WElCOME TO TASKMANAGER</h1>
-      <p>Manage your tasks efficiently!</p>
-      <div className='flex gap-3 p-10'>
-        <Button onPress={register} color='primary'>Register</Button>
-        <Button onPress={unRegister} color='danger'>un register</Button>
-      </div>
-    </div>
+    <div>page he to hai gooooooos</div>
   )
 }
